8b0eb9f8cd7c936500d146d2b0b4e9aeca663ea7
Matthieu Patou <mpatou@rockset.com>
2024-01-15 09:59:11 -0800

    Add typing to Stacky
    Summary
    
    As indicated in the title, the idea is to have things more strongly
    typed so that we can run mypy on diff to ensure that we have less risk
    of regressing.
    
    Testing
    
    Ran:
    ```
    mypy src
    ```

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
 import pathlib
+
+from setuptools import find_packages, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
@@ -7,22 +8,22 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="rockset-stacky",  
-    version="1.0.10",  
+    name="rockset-stacky",
+    version="1.0.11",
     description="""
     stacky is a tool to manage stacks of PRs. This allows developers to easily 
     manage many smaller, more targeted PRs that depend on each other.
-    """, 
-    long_description=long_description,  
-    long_description_content_type="text/markdown", 
-    url="https://github.com/rockset/stacky", 
-    author="Rockset", 
-    author_email="tudor@rockset.com", 
+    """,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rockset/stacky",
+    author="Rockset",
+    author_email="tudor@rockset.com",
     keywords="github, stack, pr, pull request",
-    package_dir={"": "src"}, 
+    package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.8, <4",
-    install_requires=["asciitree", "ansicolors", "simple-term-menu"],  
+    install_requires=["asciitree", "ansicolors", "simple-term-menu"],
     entry_points={
         "console_scripts": [
             "stacky=stacky:main",
